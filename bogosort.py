4cd9035de1e073ec9a910ef41e1a4854812e5372
ohcassanova <chel99@my.yorku.ca>
2018-02-15 13:27:39 -0500

    ???

--- bogosort.py
+++ bogosort.py
@@ -7,7 +7,7 @@ for x in range(listsize):
     num = random.randint(1, 100)
     numlist.append(num)
 
-def bogosort(listtosort): #This can't be used to check if a list is sorted
+def bogosort(listtosort): 
     count = 0
     while not all(b >= a for a, b in zip(listtosort, listtosort[1:])):
         random.shuffle(listtosort)
